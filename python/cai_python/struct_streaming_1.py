954064b4c4f6da8cf41360e63f36f7bb0cbea467
pizicai <302803946@qq.com>
2020-09-24 10:21:09 +0800

    spark struct streaming

--- python/cai_python/struct_streaming_1.py
+++ python/cai_python/struct_streaming_1.py
@@ -6,3 +6,4 @@ from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
 
 spark = pi.sql_init('testAPP', 'local[2]')
+lines = spark.readStream.format('kafka').option('kafka.bootstrap.servers', '192.168.10.10:9092').option()
